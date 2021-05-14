be202e33996d56810d5a8801bb127cb378a0d303
Robin Kupper <robin.kupper@rwth-aachen.de>
2021-05-14 15:37:16 +0200

    Added data from 13.05.21. Improved plotting.

--- biertest.py
+++ biertest.py
@@ -1,27 +1,34 @@
 from pathlib import Path
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 from visualizations import visualize_ratings, visualize_ratings_per_person, visualize_ratings_per_price
 
 
-def generate_plots(dataset, target, price=True):
+def generate_plots(dataset, target):
     # Load data
-    df = pd.read_csv(dataset, index_col=0)
+    df = pd.read_csv(dataset)
+
+    persons = [c for c in df.columns if c not in ["beer", "vol", "price", "origin"]]
 
     # Normalize ratings
-    ratings = df.values[:, 2:]
-    normalized_ratings = StandardScaler().fit_transform(ratings)
-    average_ratings = np.mean(normalized_ratings, axis=1)
+    for person in persons:
+        df[person] = (df[person] - np.mean(df[person])) / np.std(df[person])
+
+    # Melt dataframe
+    df = df.melt(id_vars=[c for c in df.columns if c not in persons], var_name='person', value_name='normalized rating')
 
     # Plot ratings
     Path(target).mkdir(parents=True, exist_ok=True)
-    visualize_ratings(target + 'ratings_average', normalized_ratings, df.index)
-    visualize_ratings_per_person(target + 'ratings_per_person', normalized_ratings, df.index, df.columns[2:])
-    if price:
-        visualize_ratings_per_price(target + 'ratings_per_price', df['price'], average_ratings, df.index)
+
+    visualize_ratings(target + 'ratings_average.png', df)
+    visualize_ratings_per_person(target + 'ratings_per_person.png', df)
+    if 'price' in df.columns:
+        visualize_ratings_per_price(target + 'ratings_per_price.png', df)
+    if 'origin' in df.columns:
+        visualize_ratings(target + 'ratings_by_origin.png', df, x='origin')
 
 
 if __name__ == "__main__":
     generate_plots('data/ratings_2019-05-01.csv', 'plots/2019-05-01/')
-    generate_plots('data/ratings_2020-06-06.csv', 'plots/2020-06-06/', price=False)
+    generate_plots('data/ratings_2020-06-06.csv', 'plots/2020-06-06/')
+    generate_plots('data/ratings_2021-05-13.csv', 'plots/2021-05-13/')
--- visualizations.py
+++ visualizations.py
@@ -1,67 +1,107 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-def visualize_ratings(file_name, ratings, labels, show=False, figsize=(16, 9)):
-    fig = plt.figure(figsize=figsize)
-    ax1 = fig.add_subplot(111)
-    ax1.set_ylabel('normalized rating')
-    ax1.grid(linestyle=':')
-
-    # Plot ratings
-    ratings = [rating[~np.isnan(rating)] for rating in ratings]  # Filter NaN values
-    ax1.boxplot(ratings, whis=[0, 100], labels=labels)
-
-    if file_name is not None:
-        #fig.savefig(file_name + ".pdf", bbox_inches='tight', pad_inches=0)
-        fig.savefig(file_name + ".png", bbox_inches='tight', pad_inches=0)
-    if show:
-        plt.show()
-    plt.close(fig)
-
-
-def visualize_ratings_per_person(file_name, ratings, labels1, labels2, show=False, figsize=(16, 9)):
-    fig = plt.figure(figsize=figsize)
-    ax1 = fig.add_subplot(111)
-    ax1.set_ylabel('normalized rating')
-    ax1.grid(linestyle=':')
-
-    # Plot ratings
-    for i in range(ratings.shape[1]):
-        ax1.scatter(labels1, ratings[:, i], label=labels2[i])
-
-    ax1.legend()
-
-    if file_name is not None:
-        #fig.savefig(file_name + ".pdf", bbox_inches='tight', pad_inches=0)
-        fig.savefig(file_name + ".png", bbox_inches='tight', pad_inches=0)
-    if show:
-        plt.show()
-    plt.close(fig)
-
-
-def visualize_ratings_per_price(file_name, prices, ratings, labels, show=False, figsize=(16, 9)):
-    fig = plt.figure(figsize=figsize)
-    ax1 = fig.add_subplot(111)
-    ax1.set_xlabel('€ / l')
-    ax1.set_ylabel('normalized rating')
-    ax1.grid(linestyle=':')
-
-    # Plot ratings
-    ax1.scatter(prices, ratings)
-    for i, label in enumerate(labels):
-        ax1.annotate(
-            label,
-            xytext=(8, -5),
-            textcoords='offset pixels',
-            xy=(prices[i], ratings[i]),
+def __add_name_labels(ax, xs, ys):
+    last_y_pos = 9999
+    for i, name in enumerate(xs):
+        y_pos = ys[i] - 0.1
+        if np.abs(y_pos - last_y_pos) < 0.1:
+            y_pos = last_y_pos - 0.1
+        last_y_pos = y_pos
+        ax.text(
+            i, y_pos, name, ha="center", va="center", bbox=dict(
+                boxstyle="round",
+                ec=(1, 1, 1, 0),
+                fc=(1, 1, 1, 0.7),
+            )
         )
 
-    ax1.set_xlim(right=2)
+    # Remove original ticks
+    ax.set_xticks([])
+    ax.set_xticks([], minor=True)
 
+
+def visualize_ratings(file_name, df, x='beer', show=False, figsize=(16, 9)):
+    fig = plt.figure(figsize=figsize)
+
+    # Plot ratings
+    ax = sns.boxplot(data=df, x=x, y='normalized rating', whis=[0, 100])
+    ax.grid(linestyle=':')
+
+    # Add nice name labels
+    __add_name_labels(ax, xs=df[x].unique(), ys=df.groupby(x, sort=False)['normalized rating'].min())
+
+    plt.tight_layout()
     if file_name is not None:
-        #fig.savefig(file_name + ".pdf", bbox_inches='tight', pad_inches=0)
-        fig.savefig(file_name + ".png", bbox_inches='tight', pad_inches=0)
+        fig.savefig(file_name)
+    if show:
+        plt.show()
+    plt.close(fig)
+
+
+def visualize_ratings_per_person(file_name, df, show=False, figsize=(16, 9)):
+    fig = plt.figure(figsize=figsize)
+
+    ax = sns.scatterplot(data=df, x='beer', y='normalized rating', hue='person', s=50, edgecolor=(0, 0, 0, 0))
+    ax.grid(linestyle=':')
+
+    # Add nice name labels
+    __add_name_labels(ax, xs=df['beer'].unique(), ys=df.groupby('beer', sort=False)['normalized rating'].min())
+
+    plt.tight_layout()
+    if file_name is not None:
+        fig.savefig(file_name)
+    if show:
+        plt.show()
+    plt.close(fig)
+
+
+def visualize_ratings_per_price(file_name, df, show=False, figsize=(16, 9)):
+    fig = plt.figure(figsize=figsize)
+
+    data = df.groupby('beer').agg(
+        price=pd.NamedAgg(column='price', aggfunc="first"),
+        rating=pd.NamedAgg(column='normalized rating', aggfunc="mean"),
+        beer=pd.NamedAgg(column='beer', aggfunc="first"),
+    )
+
+    # Plot ratings
+    ax = sns.scatterplot(
+        data=data,
+        x='price',
+        y='rating',
+        s=50,
+        color="black",
+        edgecolor=(0, 0, 0, 0),
+    )
+    ax.set_xlabel('€ / l')
+    ax.set_ylabel('normalized rating')
+    ax.grid(linestyle=':')
+
+    for _, price, rating, beer in data.itertuples():
+        ax.annotate(
+            beer,
+            xytext=(8, -5),
+            textcoords='offset pixels',
+            xy=(price, rating),
+        )
+
+    ax.set_xlim(right=2)
+
+    ax.imshow(
+        [[1, 0.5], [0.5, 0]],
+        cmap=plt.cm.RdYlGn,
+        interpolation='bicubic',
+        extent=plt.xlim() + plt.ylim(),
+        aspect="auto"
+    )
+
+    plt.tight_layout()
+    if file_name is not None:
+        fig.savefig(file_name)
     if show:
         plt.show()
     plt.close(fig)
